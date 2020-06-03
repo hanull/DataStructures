fc3cfa9e40a0790cdb18d1943377fb70fbbf46a1
hanull <dkfghg3@naver.com>
2020-06-03 20:10:21 +0900

    update: 연결리스트로 큐 구현 완료

--- queue/LinkedList_Queue.java
+++ queue/LinkedList_Queue.java
@@ -1,5 +1,63 @@
 package queue;
 
-public class LinkedList_Queue {
+import java.util.NoSuchElementException;
 
+public class LinkedList_Queue<T> implements Queue<T>{
+  private class Node<T>{
+    private T data;
+    private Node<T> next;
+    
+    public Node(T data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+  private Node<T> front, rear;
+  private int size;
+  
+  public LinkedList_Queue() {
+    front = rear = null;
+    size = 0;
+  }
+  @Override
+  public void enqueue(T data) {
+    Node<T> tmp = new Node<T>(data);
+    if (rear != null) {
+      rear.next = tmp;
+    }
+    rear = tmp;
+    size++;
+    if (front==null) {
+      front = rear;
+    }
+  }
+
+  @Override
+  public T dequeue() {
+    if (isEmpty())
+      throw new NoSuchElementException();
+    T data = peek();
+    front = front.next;
+    size--;
+    if (front==null) {
+      rear=null;
+    }
+    return data;
+  }
+
+  @Override
+  public T peek() {
+    if (isEmpty())
+      throw new NoSuchElementException();
+    return (front.data);
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return (size==0);
+  }
+  
+  public int getSize() {
+    return (size);
+  }
 }
--- queue/Main.java
+++ queue/Main.java
@@ -1,10 +1,9 @@
 package queue;
 
-import java.util.*;
-
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("--------------Array_Queue-------------------------");
     Array_Queue<Integer> arrayQueue = new Array_Queue(100);
     arrayQueue.enqueue(10);
     arrayQueue.enqueue(20);
@@ -12,7 +11,7 @@ public class Main {
     System.out.println("큐 사이즈: "+arrayQueue.getSize());
     System.out.println("dequeue : " + arrayQueue.dequeue());
     System.out.println("큐 사이즈: "+arrayQueue.getSize());
-    System.out.println("---------------------------------------");
+    System.out.println("--------------Stack_Queue-------------------------");
     Stack_Queue<Integer> stackQueue = new Stack_Queue<>();
     stackQueue.enqueue(10);
     stackQueue.enqueue(20);
@@ -21,7 +20,12 @@ public class Main {
       System.out.println("dequeue : "+stackQueue.dequeue());
     }
     System.out.println("큐 사이즈 : " + stackQueue.getSize());
-    System.out.println("---------------------------------------");
-    
+    System.out.println("--------------Linkedlist_Queue-------------------------");
+    LinkedList_Queue<Integer> linkedlistStack = new LinkedList_Queue<>();
+    linkedlistStack.enqueue(10);
+    linkedlistStack.enqueue(20);
+    System.out.println("큐 사이즈 : " + linkedlistStack.getSize());
+    System.out.println("dequeue : "+linkedlistStack.dequeue());
+    System.out.println("큐 사이즈 : " + linkedlistStack.getSize());
   }
 }
